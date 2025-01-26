42b5df50adc5a785b940d0f7d97c7a5b46d0076c
Hizbucodes <106305260+Hizbucodes@users.noreply.github.com>
2025-01-26 22:46:08 +0530

    add get product from api and display in a flatlist

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,9 +1,11 @@
 import AntDesign from "@expo/vector-icons/AntDesign";
 import Feather from "@expo/vector-icons/Feather";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import React from "react";
+import Fontisto from "@expo/vector-icons/Fontisto";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import {
+  FlatList,
   Image,
   Platform,
   Pressable,
@@ -16,6 +18,8 @@ import {
 import CustomInput from "../components/customInput/index";
 import { COMMON_COLOR } from "../constants/commonColor/index";
 import ImageCarousel from "../components/ImageCarousel";
+import axios from "axios";
+import ProductCard from "../components/product/ProductCard";
 
 const HomeScreen = () => {
   const list = [
@@ -67,6 +71,172 @@ const HomeScreen = () => {
     },
   ];
 
+  const deals = [
+    {
+      id: "20",
+      title: "OnePlus Nord CE 3 Lite 5G (Pastel Lime, 8GB RAM, 128GB Storage)",
+      oldPrice: 25000,
+      price: 19000,
+      image:
+        "https://images-eu.ssl-images-amazon.com/images/G/31/wireless_products/ssserene/weblab_wf/xcm_banners_2022_in_bau_wireless_dec_580x800_once3l_v2_580x800_in-en.jpg",
+      carouselImages: [
+        "https://m.media-amazon.com/images/I/61QRgOgBx0L._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/61uaJPLIdML._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/510YZx4v3wL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/61J6s1tkwpL._SX679_.jpg",
+      ],
+      color: "Stellar Green",
+      size: "6 GB RAM 128GB Storage",
+    },
+    {
+      id: "30",
+      title:
+        "Samsung Galaxy S20 FE 5G (Cloud Navy, 8GB RAM, 128GB Storage) with No Cost EMI & Additional Exchange Offers",
+      oldPrice: 74000,
+      price: 26000,
+      image:
+        "https://images-eu.ssl-images-amazon.com/images/G/31/img23/Wireless/Samsung/SamsungBAU/S20FE/GW/June23/BAU-27thJune/xcm_banners_2022_in_bau_wireless_dec_s20fe-rv51_580x800_in-en.jpg",
+      carouselImages: [
+        "https://m.media-amazon.com/images/I/81vDZyJQ-4L._SY879_.jpg",
+        "https://m.media-amazon.com/images/I/61vN1isnThL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/71yzyH-ohgL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/61vN1isnThL._SX679_.jpg",
+      ],
+      color: "Cloud Navy",
+      size: "8 GB RAM 128GB Storage",
+    },
+    {
+      id: "40",
+      title:
+        "Samsung Galaxy M14 5G (ICY Silver, 4GB, 128GB Storage) | 50MP Triple Cam | 6000 mAh Battery | 5nm Octa-Core Processor | Android 13 | Without Charger",
+      oldPrice: 16000,
+      price: 14000,
+      image:
+        "https://images-eu.ssl-images-amazon.com/images/G/31/img23/Wireless/Samsung/CatPage/Tiles/June/xcm_banners_m14_5g_rv1_580x800_in-en.jpg",
+      carouselImages: [
+        "https://m.media-amazon.com/images/I/817WWpaFo1L._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/81KkF-GngHL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/61IrdBaOhbL._SX679_.jpg",
+      ],
+      color: "Icy Silver",
+      size: "6 GB RAM 64GB Storage",
+    },
+    {
+      id: "50",
+      title:
+        "realme narzo N55 (Prime Blue, 4GB+64GB) 33W Segment Fastest Charging | Super High-res 64MP Primary AI Camera",
+      oldPrice: 12999,
+      price: 10999,
+      image:
+        "https://images-eu.ssl-images-amazon.com/images/G/31/tiyesum/N55/June/xcm_banners_2022_in_bau_wireless_dec_580x800_v1-n55-marchv2-mayv3-v4_580x800_in-en.jpg",
+      carouselImages: [
+        "https://m.media-amazon.com/images/I/41Iyj5moShL._SX300_SY300_QL70_FMwebp_.jpg",
+        "https://m.media-amazon.com/images/I/61og60CnGlL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/61twx1OjYdL._SX679_.jpg",
+      ],
+    },
+  ];
+
+  const offers = [
+    {
+      id: "0",
+      title:
+        "Oppo Enco Air3 Pro True Wireless in Ear Earbuds with Industry First Composite Bamboo Fiber, 49dB ANC, 30H Playtime, 47ms Ultra Low Latency,Fast Charge,BT 5.3 (Green)",
+      offer: "72% off",
+      oldPrice: 7500,
+      price: 4500,
+      image:
+        "https://m.media-amazon.com/images/I/61a2y1FCAJL._AC_UL640_FMwebp_QL65_.jpg",
+      carouselImages: [
+        "https://m.media-amazon.com/images/I/61a2y1FCAJL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/71DOcYgHWFL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/71LhLZGHrlL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/61Rgefy4ndL._SX679_.jpg",
+      ],
+      color: "Green",
+      size: "Normal",
+    },
+    {
+      id: "1",
+      title:
+        "Fastrack Limitless FS1 Pro Smart Watch|1.96 Super AMOLED Arched Display with 410x502 Pixel Resolution|SingleSync BT Calling|NitroFast Charging|110+ Sports Modes|200+ Watchfaces|Upto 7 Days Battery",
+      offer: "40%",
+      oldPrice: 7955,
+      price: 3495,
+      image: "https://m.media-amazon.com/images/I/41mQKmbkVWL._AC_SY400_.jpg",
+      carouselImages: [
+        "https://m.media-amazon.com/images/I/71h2K2OQSIL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/71BlkyWYupL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/71c1tSIZxhL._SX679_.jpg",
+      ],
+      color: "black",
+      size: "Normal",
+    },
+    {
+      id: "2",
+      title: "Aishwariya System On Ear Wireless On Ear Bluetooth Headphones",
+      offer: "40%",
+      oldPrice: 7955,
+      price: 3495,
+      image: "https://m.media-amazon.com/images/I/41t7Wa+kxPL._AC_SY400_.jpg",
+      carouselImages: ["https://m.media-amazon.com/images/I/41t7Wa+kxPL.jpg"],
+      color: "black",
+      size: "Normal",
+    },
+    {
+      id: "3",
+      title:
+        "Fastrack Limitless FS1 Pro Smart Watch|1.96 Super AMOLED Arched Display with 410x502 Pixel Resolution|SingleSync BT Calling|NitroFast Charging|110+ Sports Modes|200+ Watchfaces|Upto 7 Days Battery",
+      offer: "40%",
+      oldPrice: 24999,
+      price: 19999,
+      image: "https://m.media-amazon.com/images/I/71k3gOik46L._AC_SY400_.jpg",
+      carouselImages: [
+        "https://m.media-amazon.com/images/I/41bLD50sZSL._SX300_SY300_QL70_FMwebp_.jpg",
+        "https://m.media-amazon.com/images/I/616pTr2KJEL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/71wSGO0CwQL._SX679_.jpg",
+      ],
+      color: "Norway Blue",
+      size: "8GB RAM, 128GB Storage",
+    },
+  ];
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://fakestoreapi.com/products");
+        setProducts(response.data);
+      } catch (err) {
+        console.log(`Error Message: ${err}`);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const renderDealsItems = ({ item }) => {
+    return (
+      <Image
+        source={{ uri: item.image }}
+        resizeMode="cover"
+        style={styles.dealsItemImage}
+      />
+    );
+  };
+
+  const renderOfferItems = ({ item }) => {
+    return (
+      <View style={styles.renderOfferItems}>
+        <Image
+          source={{ uri: item.image }}
+          style={styles.dealsOfferItemImage}
+        />
+        <Text style={styles.renderOfferItemsText}>Upto {item?.offer}</Text>
+      </View>
+    );
+  };
+
   const { control } = useForm({
     defaultValues: {
       search: "",
@@ -74,7 +244,7 @@ const HomeScreen = () => {
   });
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
+      <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.searchContainer}>
           <Pressable style={styles.searchInput}>
             <AntDesign
@@ -121,6 +291,36 @@ const HomeScreen = () => {
           ))}
         </ScrollView>
         <ImageCarousel />
+        <Text style={styles.trendingDealsText}>
+          Trending Deals of the week{" "}
+          <Fontisto name="fire" size={24} color="#FFC11F" />
+        </Text>
+
+        <View style={styles.dealsItemContainer}>
+          <FlatList
+            data={deals}
+            renderItem={renderDealsItems}
+            keyExtractor={(deal) => deal.id}
+            numColumns={2}
+          />
+        </View>
+
+        <Text style={styles.todaysDealsText}>Today's Deals</Text>
+        <View style={styles.todaysDealsContainer}>
+          <FlatList
+            data={offers}
+            renderItem={renderOfferItems}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+          />
+        </View>
+
+        <View>
+          <Text style={styles.productCategoryHeaderTitle}>
+            Find Products By Category
+          </Text>
+          <ProductCard products={products} />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -178,5 +378,55 @@ const styles = StyleSheet.create({
     fontWeight: 500,
     fontSize: 12,
     textAlign: "center",
+  },
+  trendingDealsText: {
+    fontWeight: "bold",
+    fontSize: 22,
+    paddingLeft: 20,
+    marginVertical: 25,
+  },
+  dealsItemContainer: {
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  dealsItemImage: {
+    marginHorizontal: 15,
+    width: 200,
+    height: 300,
+  },
+  todaysDealsText: {
+    fontWeight: "bold",
+    fontSize: 22,
+    paddingLeft: 20,
+    marginVertical: 25,
+  },
+  todaysDealsContainer: {
+    marginBottom: 20,
+  },
+  dealsOfferItemImage: {
+    marginHorizontal: 15,
+    width: 150,
+    resizeMode: "center",
+    height: 200,
+  },
+  renderOfferItems: {
+    flexDirection: "column",
+    alignItems: "center",
+    rowGap: 10,
+  },
+  renderOfferItemsText: {
+    width: "90%",
+    backgroundColor: "#d42b49",
+    padding: 10,
+    fontWeight: "bold",
+    color: "white",
+    borderRadius: 5,
+    textAlign: "center",
+  },
+  productCategoryHeaderTitle: {
+    fontWeight: "bold",
+    fontSize: 22,
+    paddingLeft: 20,
+    marginVertical: 25,
   },
 });
